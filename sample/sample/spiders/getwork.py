fcd6fdc189ffba6054c2a789524eb56ba85aa4e5
500apps-Pravallikakollipara <pravallika.kollippara@500apps.com>
2023-04-20 19:17:38 +0530

    Added code in getworks

--- sample/sample/spiders/getwork.py
+++ sample/sample/spiders/getwork.py
@@ -12,10 +12,21 @@ class GetworkSpider(scrapy.Spider):
     def parse(self, response):
             job_title = response.css('h5[class="MuiTypography-root jss49 textOverflow MuiTypography-h5 MuiTypography-colorPrimary"]::text').extract()
             company_name = response.css('p[class="MuiTypography-root jss49 textOverflow MuiTypography-body1 MuiTypography-colorPrimary"]::text').extract()
-            # job_loctaion = response.css('div[class="jss52"]::text"]').extract()
+            job_designation = response.css('p[class="MuiTypography-root jss55 MuiTypography-body2 MuiTypography-colorPrimary MuiTypography-noWrap"]::text').extract()
+            job_experience = response.css('p[class="MuiTypography-root jss53 MuiTypography-body2"]::text').extract()
+            job_salary = response.xpath("//div[@style='margin-right:15px' and @class='jss44']//text()").extract()
+            job_info = response.css('p[class="MuiTypography-root jss53 MuiTypography-body2 MuiTypography-colorPrimary"]::text').extract()
+            # job_type = response.xpath("//div[@style='margin-right: 25px;']//p//span//text()").extract()
+            job_location = response.xpath("//ul//p[@class='MuiTypography-root MuiTypography-body2 MuiTypography-colorPrimary']/text()").extract()
+
             for j in range(len(job_title)):
                 yield {
                 "job_title" : job_title[j].strip(),
-                "comapny_name" : company_name[j].strip()
-                # "job_location" : job_loctaion[j].strip()
-            }
+                "comapny_name" : company_name[j].strip(),
+                "job_designation" : job_designation[j].strip(),
+                "job_experience" : job_experience[j].strip(),
+                "job_salary" : job_salary[j].strip(),
+                "job_info" : job_info[j].strip(),
+                # "job_type" : job_type[j].strip(),
+                "job_location" : job_location[j].strip()  if j < len(job_location) else ''
+            }   
